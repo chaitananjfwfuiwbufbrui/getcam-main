5e30cd4b75cadb25e9ae60b13d8aea353bfb1458
chaitanya <kc508275@gmail.com>
2021-03-31 22:21:20 +0530

    sf

--- getcam/getcam/settings.py
+++ getcam/getcam/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #install apps
     'authentications',
     'main',
+    'merchant',
 
 
 
--- getcam/getcam/urls.py
+++ getcam/getcam/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('merchant/', include('merchant.urls')),
     path('auth/', include('authentications.urls')),
     path('swad/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
